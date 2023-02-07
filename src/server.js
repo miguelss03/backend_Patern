6d33deeffc5443c469efef46ab45d1a5f9b310a0
Miguel S. Silva <miguelprogs.silva@gmail.com>
2023-02-07 01:15:33 -0300

    aula 02, rtoas de criacao e listagem

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import http from 'node:http';
 
-const server = http.createServer((req, res) => {
+const users = []
 
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+        return res.end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: "FULANO DE TAL",
+            email: "fulano@gmail.com"
+        })
+
+        return res.end('CRIAÇÃO DE USUÁRIOS')
+    }
+
+    return res.end('EAI IGNITE NODE')
 })
 
 server.listen(3434)
