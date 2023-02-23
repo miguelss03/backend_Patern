66250ff469946cbcd80cc5a95737008cf22538df
Miguel S. Silva <miguelprogs.silva@gmail.com>
2023-02-23 20:19:24 -0300

    Streaming de leitura e tranformacao

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,7 @@
-import { Readable } from 'node:stream';
+
+
+
+import { Readable, Writable, Transform, Duplex } from 'node:stream';
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -14,12 +17,29 @@ class OneToHundredStream extends Readable {
 
                 this.push(buf)
             }
-        }, 500)
+        }, 1000)
 
     }
 }
 
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
 new OneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
+
 
 
