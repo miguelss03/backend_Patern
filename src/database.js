e84432f7ac64d6282e7a90f129112389bdfd2b24
Miguel S. Silva <miguelprogs.silva@gmail.com>
2023-03-10 00:22:27 -0300

    banco de dados e persistencia

--- src/database.js
+++ src/database.js
@@ -1,10 +1,21 @@
 import fs from 'node:fs/promises'
 
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
     #database = {}
 
-    #persist() {
+    constructor() {
+        fs.readFile(databasePath, 'utf8').then(data => {
+            this.#database = JSON.parse(data);
+        })
+            .catch(() => {
+                this.#persist()
+            })
+    }
 
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
     select(table) {
@@ -18,6 +29,8 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+
+        this.#persist()
 
         return data;
     }
